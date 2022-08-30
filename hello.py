e362222876295fe25f534b523df28c9214b161e5
Ben Moser <pbenmoser4@gmail.com>
2022-08-30 00:49:38 +0000

    reading

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
-def add(x, y):
-    return x + y
-
-# var=
-result = add(1, 2)
-print("This is the sum: 1, 2, {}".format(result))
+def toyou(x):
+    return "hi {}".format(x)
+    
+def add(x):
+    return x+1
+    
+def subtract(x):
+    return x-1
--- test_hello.py
+++ test_hello.py
@@ -1,4 +1,12 @@
-from hello import add
+from hello import toyou, add, subtract
 
-def test_add():
-    assert add(1,2) == 3
+def setup_function(function):
+    print(" Running setup: {}".format(function.__name__))
+    function.x = 10
+    
+def teardown_function(function):
+    print(" Running teardown: {}".format(function.__name__))
+    del function.x
+    
+def test_hello_subtract():
+    assert subtract(test_hello_subtract.x) == 9
